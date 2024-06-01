511312eec0b274c2dd698143508c34799e4ce150
Lalit-threely <147034093+Lalit-threely@users.noreply.github.com>
2024-06-01 12:17:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -44,6 +44,19 @@ app.all("*", (req, res, next) => {
 
 app.use(errorHandler);
 
+function hitApi() {
+  axios
+    .get("http://127.0.0.1:10000/")
+    .then((response) => {
+      console.log("API response:", response.data);
+    })
+    .catch((error) => {
+      console.error("Error hitting API:", error);
+    });
+}
+hitApi();
+setInterval(hitApi, 25 * 1000);
+
 const PORT = 8080;
 
 app.listen(PORT, () => {
