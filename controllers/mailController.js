ed151e0e54ef6eb7858c6c4cdcc7593c460b6618
Yuvraj Singh Wahla <yuvrajsinghwahla08cs@gmail.com>
2024-06-02 22:00:50 +0530

    Added Enquiry Form API

--- controllers/mailController.js
+++ controllers/mailController.js
@@ -35,4 +35,27 @@ const joinUs = async (req, res) => {
     }
 }
 
-module.exports = { joinUs };
+const enquiry = async (req, res) => {
+    try {
+        const { name, contact, country, productCategory, productName, productQuantity, message } = req.body;
+        const mailOptions = {
+            from: contact,
+            to: 'anshyuve@gmail.com', // The recipient's email address
+            subject: `${name} wants to enquire about ${productName}`,
+            text: `Name: ${name}\nContact: ${contact}\nCountry: ${country}\nProduct Category: ${productCategory}\nProduct Name: ${productName}\nProduct Quantity: ${productQuantity}\nAdditional Comments: ${message}`
+        };
+
+        // Send the email
+        transporter.sendMail(mailOptions, (error, info) => {
+            if (error) {
+                return res.status(500).json({ error, success:false });
+            }
+            res.status(200).json({ message: 'Email sent successfully', success: true });
+        });
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({ error: error, success:false });
+    }
+}
+
+module.exports = { joinUs, enquiry };
--- routes/mailRoutes.js
+++ routes/mailRoutes.js
@@ -3,6 +3,7 @@ const mailController = require('../controllers/mailController');
 
 const router = Router();
 
+router.post('/enquiry', mailController.enquiry)
 router.post('/join-us', mailController.joinUs);
 
 module.exports = router;
